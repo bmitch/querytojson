5ebd14be39958c91f3bef109018d9b4669ac02e2
Bill Mitchell <wkmitch@gmail.com>
2015-06-27 08:43:09 +0100

    Moved files

---
+++ source.js
@@ -0,0 +1,5 @@
+$('input[type="button"]').click(function() {
+  var qs = $('#code').val();
+  qs = $.parseJSON('{"' + qs.replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+  $('#result').val(JSON.stringify(qs));
+});
