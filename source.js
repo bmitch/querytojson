792b6ffd450758d68ad058773d5ebd0c9c15ada4
Bill Mitchell <wkmitch@gmail.com>
2015-06-27 08:55:57 +0100

    Added jQ ready

--- source.js
+++ source.js
@@ -1,5 +1,9 @@
-$('input[type="button"]').click(function() {
-  var qs = $('#code').val();
-  qs = $.parseJSON('{"' + qs.replace(/&/g, '","').replace(/=/g, '":"') + '"}');
-  $('#result').val(JSON.stringify(qs));
+$( document ).ready(function() {
+
+   $('input[type="button"]').click(function() {
+      var qs = $('#code').val();
+      qs = $.parseJSON('{"' + qs.replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+      $('#result').val(JSON.stringify(qs));
+   });
+
 });
